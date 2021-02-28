c11670229b2c06707ae8e5044331bb3e28fa8d49
kardelio <kardeliogithub@outlook.com>
2021-02-28 10:41:37 +0100

    Minor tweeks

--- public/add.js
+++ public/add.js
@@ -37,6 +37,7 @@ function clickSingleGameItem(id) {
     addAGame(getGameFromGamesList(id))
         .then(d => {
             console.log(d);
+            //add.js:39 {success: true, message: "Game added to list"}
         })
 }
 
@@ -47,10 +48,6 @@ function getGameFromGamesList(id) {
     })
 }
 
-//name
-//url
-//id
-//rank
 function searchButtonClick() {
     document.getElementById("searched-game-container").innerHTML = "";
     let query = document.getElementById("game-search-text-input").value;
@@ -68,8 +65,7 @@ function searchButtonClick() {
     }
 }
 
-// const example = { "games": [{ "name": "Eclipse", "url": "/boardgame/72125/eclipse", "rank": "51", "id": "72125" }, { "name": "Eclipse  Second Dawn for the Galaxy", "url": "/boardgame/246900/eclipse-second-dawn-galaxy", "rank": "171", "id": "246900" }, { "name": "Hapsburg Eclipse", "url": "/boardgame/149361/hapsburg-eclipse", "rank": "3823", "id": "149361" }, { "name": "Eclipse", "url": "/boardgame/11542/eclipse", "rank": "9952", "id": "11542" }, { "name": "The Twilight Saga  Eclipse  The Movie Board Game", "url": "/boardgame/69827/twilight-saga-eclipse-movie-board-game", "rank": "NA", "id": "69827" }, { "name": "Kontact Now  Red Eclipse", "url": "/boardgame/184763/kontact-now-red-eclipse", "rank": "NA", "id": "184763" }, { "name": "Eclipse", "url": "/boardgame/23272/eclipse", "rank": "NA", "id": "23272" }, { "name": "HitchHikers Guide to the Eclipse", "url": "/boardgame/35700/hitch-hikers-guide-eclipse", "rank": "NA", "id": "35700" }, { "name": "Epic Roll  Eclipse", "url": "/boardgame/209700/epic-roll-eclipse", "rank": "NA", "id": "209700" }, { "name": "Eclipse Tiddlywinks", "url": "/boardgame/4792/tiddledy-winks", "rank": "20211", "id": "4792" }, { "name": "Rubiks Eclipse", "url": "/boardgame/3081/rubiks-flip", "rank": "12916", "id": "3081" }, { "name": "Eclipse", "url": "/boardgame/824/hijara", "rank": "16005", "id": "824" }, { "name": "Eclipse", "url": "/boardgame/8148/trio", "rank": "NA", "id": "8148" }] };
-const example = {"games": [{"name": "Eclipse", "url": "/boardgame/72125/eclipse", "rank": "51", "year": "2011", "id": "72125"}, {"name": "Eclipse  Second Dawn for the Galaxy", "url": "/boardgame/246900/eclipse-second-dawn-galaxy", "rank": "162", "year": "2020", "id": "246900"}, {"name": "Hapsburg Eclipse", "url": "/boardgame/149361/hapsburg-eclipse", "rank": "3841", "year": "2014", "id": "149361"}, {"name": "Eclipse", "url": "/boardgame/11542/eclipse", "rank": "9961", "year": "1999", "id": "11542"}, {"name": "The Twilight Saga  Eclipse  The Movie Board Game", "url": "/boardgame/69827/twilight-saga-eclipse-movie-board-game", "rank": "NA", "year": "2010", "id": "69827"}, {"name": "Kontact Now  Red Eclipse", "url": "/boardgame/184763/kontact-now-red-eclipse", "rank": "NA", "year": "N/A", "id": "184763"}, {"name": "Eclipse", "url": "/boardgame/23272/eclipse", "rank": "NA", "year": "N/A", "id": "23272"}, {"name": "HitchHikers Guide to the Eclipse", "url": "/boardgame/35700/hitch-hikers-guide-eclipse", "rank": "NA", "year": "1999", "id": "35700"}, {"name": "Epic Roll  Eclipse", "url": "/boardgame/209700/epic-roll-eclipse", "rank": "NA", "year": "2017", "id": "209700"}, {"name": "Eclipse Tiddlywinks", "url": "/boardgame/4792/tiddledy-winks", "rank": "20234", "year": "1888", "id": "4792"}, {"name": "Rubiks Eclipse", "url": "/boardgame/3081/rubiks-flip", "rank": "12929", "year": "1981", "id": "3081"}, {"name": "Eclipse", "url": "/boardgame/824/hijara", "rank": "16022", "year": "1995", "id": "824"}, {"name": "Eclipse", "url": "/boardgame/8148/trio", "rank": "NA", "year": "1972", "id": "8148"}]}
+// const example = {"games": [{"name": "Eclipse", "url": "/boardgame/72125/eclipse", "rank": "51", "year": "2011", "id": "72125"}, {"name": "Eclipse  Second Dawn for the Galaxy", "url": "/boardgame/246900/eclipse-second-dawn-galaxy", "rank": "162", "year": "2020", "id": "246900"}, {"name": "Hapsburg Eclipse", "url": "/boardgame/149361/hapsburg-eclipse", "rank": "3841", "year": "2014", "id": "149361"}, {"name": "Eclipse", "url": "/boardgame/11542/eclipse", "rank": "9961", "year": "1999", "id": "11542"}, {"name": "The Twilight Saga  Eclipse  The Movie Board Game", "url": "/boardgame/69827/twilight-saga-eclipse-movie-board-game", "rank": "NA", "year": "2010", "id": "69827"}, {"name": "Kontact Now  Red Eclipse", "url": "/boardgame/184763/kontact-now-red-eclipse", "rank": "NA", "year": "N/A", "id": "184763"}, {"name": "Eclipse", "url": "/boardgame/23272/eclipse", "rank": "NA", "year": "N/A", "id": "23272"}, {"name": "HitchHikers Guide to the Eclipse", "url": "/boardgame/35700/hitch-hikers-guide-eclipse", "rank": "NA", "year": "1999", "id": "35700"}, {"name": "Epic Roll  Eclipse", "url": "/boardgame/209700/epic-roll-eclipse", "rank": "NA", "year": "2017", "id": "209700"}, {"name": "Eclipse Tiddlywinks", "url": "/boardgame/4792/tiddledy-winks", "rank": "20234", "year": "1888", "id": "4792"}, {"name": "Rubiks Eclipse", "url": "/boardgame/3081/rubiks-flip", "rank": "12929", "year": "1981", "id": "3081"}, {"name": "Eclipse", "url": "/boardgame/824/hijara", "rank": "16022", "year": "1995", "id": "824"}, {"name": "Eclipse", "url": "/boardgame/8148/trio", "rank": "NA", "year": "1972", "id": "8148"}]}
 
 function navigateToAllGames() {
     window.location.href = "/";
@@ -90,7 +86,6 @@ function getAllGames() {
 }
 
 function addAGame(gameObject) {
-    // let gameout = JSON.stringify(gameObject);
     return new Promise((res, rej) => {
         fetch("/addAGame", {
             method: 'POST',
@@ -112,7 +107,6 @@ function addAGame(gameObject) {
 
 function searchForGame(query) {
     return new Promise((res, rej) => {
-        // res(example);
         fetch(`/search?query=${query}`)
             .then((data) => {
                 res(data.json())
@@ -121,11 +115,3 @@ function searchForGame(query) {
             })
     });
 }
-/*
- style="background-image: url('${game.image}')"
-
-background-position: center;
-    background-size: contain;
-    background-repeat: no-repeat;
-    background-color: black;
-*/
